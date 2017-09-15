bf90a1f3798791a32dea20b1692c3347cd7d6d64
Thuan Duong <thuan.duong@titancorpvn.com>
2017-09-15 14:33:54 +0700

    Refactor code in Keyword.

--- src/test/java/com/auvenir/ui/bdd/pages/auditor/AuditorDetailsEngagementPage.java
+++ src/test/java/com/auvenir/ui/bdd/pages/auditor/AuditorDetailsEngagementPage.java
@@ -109,7 +109,7 @@ public class AuditorDetailsEngagementPage extends DetailsEngagementPage {
      */
     public void clickInviteClientButton() throws Exception {
         validateElementText(buttonInviteClient, "Invite Client");
-        clickByJavaScripts(buttonInviteClient, "Button Invite Client");
+        clickElement(buttonInviteClient, "Button Invite Client");
     }
 
     @FindBy(className = "m-ic-subTitle")
