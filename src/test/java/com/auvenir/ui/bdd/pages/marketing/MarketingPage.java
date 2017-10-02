e3944fa50df299148bb9fb9130f4ee09dbd32187
HuyHuynh <huy.huynh@titancorpvn.com>
2017-10-02 14:48:04 +0700

    done AUV-548, AUV-550

--- src/test/java/com/auvenir/ui/bdd/pages/marketing/MarketingPage.java
+++ src/test/java/com/auvenir/ui/bdd/pages/marketing/MarketingPage.java
@@ -31,6 +31,30 @@ public class MarketingPage extends CommonPage {
     @FindBy(id = "marketing-header")
     private WebElement marketingHeader;
 
+    @FindBy(xpath = "//a[@class='ui label forgot-password']")
+    private WebElement linkForgotPassword;
+
+    @FindBy(xpath = "//div[@id='forgot-popup']//input[@name='email']")
+    private WebElement inputForgottenEmail;
+
+    @FindBy(xpath = "//div[@id='forgot-popup']//button[@type='submit']")
+    private WebElement buttonRequestResetLink;
+
+    @FindBy(xpath = "//div[@id='waitlist-popup']//span")
+    private WebElement spanAccountStillProcessingMessage;
+
+    @FindBy(className = "login-title")
+    private WebElement titleLogin;
+
+    @FindBy(id = "forgot-popup")
+    private WebElement popupForgotPassword;
+
+    @FindBy(xpath = "//div[@id='footer']//a[@href='/about']")
+    private WebElement itemFooterAbout;
+
+    @FindBy(xpath = "//div[contains(@class,'about-header')]")
+    private WebElement textBannerInformation;
+
     //private String xpathStatusCellOnUserTableAdminX = "//td[text()='%s']/ancestor::tr/td[5]/select";
 
     public MarketingPage(Logger logger, WebDriver driver) {
@@ -60,4 +84,81 @@ public class MarketingPage extends CommonPage {
         Assert.assertTrue(result, "Verify marketing header page.");
     }
 
+    public void clickOnForgotPasswordLink() {
+        //            waitForClickableOfElement(forgotPasswordLink, "forgotPasswordLink");
+        clickElement(linkForgotPassword, "Link Forgot Pasword");
+    }
+
+    public void verifyForgotPasswordPopup() {
+        validateExistedElement(popupForgotPassword, "Popup Forgot Password");
+    }
+
+    public void inputEmailForgottenPassword(String email) {
+        sendKeyTextBox(inputForgottenEmail, email, "Input Forgotten Email");
+    }
+
+    public void clickOnRequestResetLinkButton() {
+        clickElement(buttonRequestResetLink, "Button Request Reset Link");
+    }
+
+    public void verifyStillProcessingMessage() {
+        validateElementText(spanAccountStillProcessingMessage,
+                "Thanks for registering for the Auvenir platform! Your account information is being reviewed and you"
+                        + " will receive an email from our customer service team shortly. If you have any questions "
+                        + "in the meantime, please send us a note at info@auvenir.com.");
+    }
+
+    public void verifyLoginPopupTitle() {
+        validateElementText(titleLogin, "Login to Auvenir");
+    }
+
+    @FindBy(xpath = "//img[contains(@class,'ui-logo')]")
+    private WebElement imageLogo;
+
+    @FindBy(xpath = "//button[contains(@class,'btn-login')]")
+    private WebElement linkLogin;
+
+    @FindBy(xpath = "//div[@class='row header-logo']//a[contains(@class,'inverted button')]")
+    private WebElement linkAuditorSignUp;
+
+    @FindBy(xpath = "//div[contains(@class,'button inverted')]")
+    private WebElement linkLanguage;
+
+    @FindBy(xpath = "//div[@class='row']//a[contains(@class,'inverted button')]")
+    private WebElement linkJoinAsAuditor;
+
+    public void verifyHeaderLogo() {
+        boolean result = validateAttributeContain(imageLogo, "src", "static/images/logo-auvenir.svg", "Image Logo");
+        Assert.assertTrue(result, "Link Header Logo should be: " + "static/images/logo-auvenir.svg");
+    }
+
+    public void verifyLoginLink() {
+        boolean result = validateExistedElement(linkLogin, "Link Login");
+        Assert.assertTrue(result, "Login Link should be exist");
+    }
+
+    public void seeSignUpLink() {
+        boolean result = validateExistedElement(linkAuditorSignUp, "Link Auditor Sign Up");
+        Assert.assertTrue(result, "Link Auditor Sign Up should be exist");
+    }
+
+    public void seeLanguageLink() {
+        boolean result = validateExistedElement(linkLanguage, "Link Language");
+        Assert.assertTrue(result, "Link Language should be exist");
+    }
+
+    public void seeJoinAsAuditorLink() {
+        boolean result = validateExistedElement(linkJoinAsAuditor, "Link Join As Auditor");
+        Assert.assertTrue(result, "Link Join As Auditor should be exist");
+    }
+
+    public void redirectToAboutPage() {
+        scrollToFooter();
+        clickElement(itemFooterAbout, "Item Footer About");
+    }
+
+    public void seeBannerInformation() {
+        boolean result = validateExistedElement(textBannerInformation, "Banner Information");
+        Assert.assertTrue(result, "Banner Information should be exist");
+    }
 }
